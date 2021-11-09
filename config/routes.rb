a6cfdffe82345615d0b244d1bc524d02671c018f
Derek Torres <derek.n.torres@gmail.com>
2021-11-09 11:07:58 +0900

    cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'tasks#index', as: 'tasks'
 
   get 'tasks/new', to: 'tasks#new', as: 'new'
